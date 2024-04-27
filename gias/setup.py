746bb16967cd029c5bde127f6e7bfd2026ccf8cb
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-04-27 14:15:12 +0100

    Module Structure Created

---
+++ gias/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+
+setup(
+    name="py-GIAS",
+    version="0.1",
+    description="Query the DFE Get Information About Schools dataset using Python",
+    url="https://github.com/xn4224nx/py-GIAS/",
+    author="Benjamin Mouncer",
+    author_email="benjamin.dafydd.mouncer@gmail.com",
+    license="GPL-3.0",
+    packages=["gias"],
+    zip_safe=False,
+)
