5aa2d486358ab4da798d67a160fbf5e7023f32d4
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-04-27 16:45:07 +0100

    API credential retrieval and testing.

--- gias/scrape.py
+++ gias/scrape.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import requests
+import os
 
 
 class API:
@@ -9,17 +10,42 @@ class API:
     """
 
     def __init__(self):
-        proxy = {
+        self.proxy = {
             "http": None,
             "https": None,
             "ftp": None,
         }
+
+        self.credentials = {
+            "GOV_APP_NAME": None,
+            "GOV_CLIENT_ID": None,
+            "GOV_PRI_SEC": None,
+            "GOV_SEC_SEC": None,
+        }
+
+        self.gov_url = "https://oat-api-customerengagement.platform.education.gov.uk"
+        self.endpt = "dfe/open-info"
 
     def get(self, url: str):
         """
         Make a get request.
         """
 
-        api_req = requests.get(url)
+        api_req = requests.get(url, proxies=self.proxy)
 
         return api_req
+
+    def populate_credentials(self):
+        """
+        Access the users environmental variables to get the credentials to
+        access the DFE API.
+        """
+        users_env_var = os.environ
+
+        for cred_name in self.credentials:
+            if cred_name not in users_env_var:
+                raise Exception(
+                    f"The credential '{cred_name}' has not been set in your environment!"
+                )
+            else:
+                self.credentials[cred_name] = users_env_var[cred_name]
--- gias/unit_tests/scrape_tests.py
+++ gias/unit_tests/scrape_tests.py
@@ -15,3 +15,14 @@ def test_api_get():
     assert result.status_code == 200
     assert type(result.content == str)
     assert result.content is not None
+
+
+def test_credential_acquisition():
+    """
+    Ensure the credentials can be obtained from the environmental variables
+    or another source.
+    """
+    endpt = scrape.API()
+    endpt.populate_credentials()
+
+    assert None not in endpt.credentials.values()
